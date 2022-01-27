dfdf64873a9e88200f23613e53215c3088350f79
Robert <robert.ewald@uis.no>
2022-01-27 16:33:17 +0100

    Add Material for lecture Java Script Part I. Basics

---
+++ examples/js/basics/dog.class.js
@@ -0,0 +1,24 @@
+/* Dog class - to be used in objects.html */
+"use strict";
+
+function Dog(name, weight, breed) {
+    this.name = name;
+    this.weight = weight;
+    this.breed = breed;
+}
+
+function printInfo() {
+    console.log("name:   " + this.name);
+    console.log("weight: " + this.weight);
+    console.log("breed:  " + this.breed);
+}
+
+// Adding an info() method to the Dog prototype
+Dog.prototype.info = printInfo;
+
+let mydog = new Dog("Tiffy", 3.4, "mixed");
+mydog.info();
+
+if (mydog instanceof Dog) {
+    console.log("it's a dog");
+}
